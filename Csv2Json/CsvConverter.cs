a2c84e500a146d177fa5727003c8c68fb6d18738
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2021-02-08 12:57:26 +0600

    Refactor by moving code to a class and updating sample data.

---
+++ Csv2Json/CsvConverter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+
+namespace Csv2Json
+{
+    public static class CsvConverter
+    {
+        public static string ConvertToJson(string fileName)
+        {
+            var objectCollection = new Dictionary<string, Dictionary<string, string>>();
+            using var reader = new StreamReader(fileName);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var lastObjectName = "";
+            while (csv.Read())
+            {
+                var objectName = csv[0];
+                var propertyName = csv[1];
+                var propertyValue = csv[2];
+                if (!string.IsNullOrWhiteSpace(objectName))
+                {
+                    lastObjectName = objectName;
+                    AddCountry(objectCollection, objectName);
+                }
+
+                if (!string.IsNullOrWhiteSpace(propertyName))
+                {
+                    AddHoliday(objectCollection, lastObjectName, propertyName, propertyValue);
+                }
+            }
+
+            return Newtonsoft.Json.JsonConvert.SerializeObject(objectCollection);
+        }
+
+        private static void AddHoliday(Dictionary<string, Dictionary<string, string>> objectCollection, string objectName, string propertyName, string propertyValue)
+        {
+            AddCountry(objectCollection, objectName);
+            var existingHolidays = objectCollection[objectName];
+
+            if (existingHolidays.ContainsKey(propertyName)) return;
+
+            existingHolidays[propertyName] = propertyValue;
+        }
+
+        private static void AddCountry(Dictionary<string, Dictionary<string, string>> objectCollection, string objectName)
+        {
+            if (objectCollection.ContainsKey(objectName)) return;
+            objectCollection.Add(objectName, new Dictionary<string, string>());
+        }
+    }
+}
--- Csv2Json/Program.cs
+++ Csv2Json/Program.cs
@@ -1,8 +1,4 @@
-﻿using CsvHelper;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
+﻿using System;
 
 namespace Csv2Json
 {
@@ -22,55 +18,13 @@ namespace Csv2Json
 
             try
             {
-                var json = ConvertToJson(fileName);
+                var json = CsvConverter.ConvertToJson(fileName);
                 ShowJson(json);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-        }
-
-        static string ConvertToJson(string fileName)
-        {
-            var objectCollection = new Dictionary<string, Dictionary<string, string>>();
-            using var reader = new StreamReader(fileName);
-            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-            var lastObjectName = "";
-            while (csv.Read())
-            {
-                var objectName = csv[0];
-                var propertyName = csv[1];
-                var propertyValue = csv[2];
-                if (!string.IsNullOrWhiteSpace(objectName))
-                {
-                    lastObjectName = objectName;
-                    AddCountry(objectCollection, objectName);
-                }
-
-                if (!string.IsNullOrWhiteSpace(propertyName))
-                {
-                    AddHoliday(objectCollection, lastObjectName, propertyName, propertyValue);
-                }
-            }
-
-            return Newtonsoft.Json.JsonConvert.SerializeObject(objectCollection);
-        }
-
-        private static void AddHoliday(Dictionary<string, Dictionary<string, string>> objectCollection, string objectName, string propertyName, string propertyValue)
-        {
-            AddCountry(objectCollection, objectName);
-            var existingHolidays = objectCollection[objectName];
-
-            if (existingHolidays.ContainsKey(propertyName)) return;
-
-            existingHolidays[propertyName] = propertyValue;
-        }
-
-        private static void AddCountry(Dictionary<string, Dictionary<string, string>> objectCollection, string objectName)
-        {
-            if (objectCollection.ContainsKey(objectName)) return;
-            objectCollection.Add(objectName, new Dictionary<string, string>());
         }
 
         static void ShowJson(string json)
